5d1cb0161ab714ddc6a971a7bf952131b17e7994
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-05-13 20:08:24 -0400

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['index.js', 'lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      },
+      examples: {
+        src: ['examples/**/*.js']
+      }
+    },
+    regarde: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile'],
+        spawn: true
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'simplemocha'],
+        spawn: true
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'simplemocha'],
+        spawn: true
+      },
+    },
+
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        //grep: '*-test',
+        ui: 'bdd',
+        reporter: 'list'
+      },
+
+      all: { src: ['<%= jshint.test.src %>'] }
+    }
+  });
+
+    // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-regarde');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('test', ['simplemocha']);
+    // Default task.
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+};
---
+++ schema.js
@@ -0,0 +1,51 @@
+var vogels = require('vogels');
+
+var Account = vogels.define('Account', function (schema) {
+  schema.String('email', {hashKey: true});
+  schema.String('username').alphanum().min(3).max(30).required();
+  schema.String('name').required();
+  schema.Number('birthYear').min(1850).max(2012);
+});
+
+var BlogPost = vogels.define('BlogPost', function (schema) {
+  schema.String('email', {hashKey: true});
+  schema.String('title', {rangeKey: true});
+  schema.Date('published', {secondaryIndex: true});
+  schema.String('content').required();
+});
+
+var acc = new Account({
+  email : 'vogels@test.com',
+  username : 'werner',
+  name : 'Werner Vogels',
+  birthYear: 1958
+});
+
+var publishedPost = new BlogPost({
+  email : acc.email,
+  title : 'Expanding the Cloud: Faster, More Flexible Queries with DynamoDB',
+  content : 'Today, I’m thrilled to announce that we have expanded the query capabilities of DynamoDB.',
+  published : new Date('2013-04-17 10:30:00')
+});
+
+var unpublishedPost = new BlogPost({
+  email : acc.email,
+  title : 'Expanding the Cloud: Spaceships!',
+  content : 'Today, I’m thrilled to announce...',
+});
+
+acc.save(function (err){
+  if(err) {console.log('error saving account', err);}
+
+  // account created!
+});
+
+Account.get('vogels@test.com', function (err, account) {
+  if(err) {console.log('error getting account', err);}
+
+  // got account
+});
+
+BlogPost.query('vogels@test.com').exec(callback); // Get all blog posts created by vogels
+
+BlogPost.query('vogels@test.com').usingIndex('published').exec(callback); // Get all published blogposts by vogels
