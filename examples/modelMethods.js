7399aa2981e64288ae63567818688ce7c8306937
Ryan Fitzgerald <ryan@codebrewstudios.com>
2013-09-06 12:34:58 -0400

    adding example to demonstrate adding methods to model objects

---
+++ examples/modelMethods.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var vogels = require('../index'),
+AWS    = vogels.AWS;
+
+AWS.config.loadFromPath(process.env.HOME + '/.ec2/credentials.json');
+
+var Account = vogels.define('Account', function (schema) {
+  schema.String('email', {hashKey: true});
+  schema.String('name');
+  schema.Number('age');
+  schema.Date('created', {default: Date.now});
+});
+
+Account.prototype.sayHello = function () {
+  console.log('Hello my name is ' + this.get('name') + ' I\'m ' + this.get('age') + ' years old');
+};
+
+Account.findByAgeRange = function (low, high) {
+  Account.scan()
+  .where('age').gte(low)
+  .where('age').lte(high)
+  .loadAll()
+  .exec(function (err, data) {
+    data.Items.forEach(function (account) {
+      account.sayHello();
+    });
+  });
+};
+
+
