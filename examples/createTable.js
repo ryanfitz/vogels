faf0591d7ce305417ab3ad2db2efa2c9b4321e5f
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2013-12-20 18:41:40 -0500

    adding ability to create all defined tables

--- examples/createTable.js
+++ examples/createTable.js
@@ -5,22 +5,48 @@ var vogels = require('../index'),
 
 AWS.config.loadFromPath(process.env.HOME + '/.ec2/credentials.json');
 
-var Account = vogels.define('Account', function (schema) {
+vogels.define('Account', function (schema) {
   schema.String('name', {hashKey: true});
   schema.String('email', {rangeKey: true});
   schema.Number('age', {secondaryIndex: true});
   schema.Date('created', {default: Date.now});
 });
 
-Account.createTable(function (err, result) {
+vogels.define('GameScore', function (schema) {
+  schema.String('userId', {hashKey: true});
+  schema.String('gameTitle', {rangeKey: true});
+  schema.Number('topScore');
+  schema.Date('topScoreDateTime');
+  schema.Number('wins');
+  schema.Number('losses');
+
+  schema.globalIndex('GameTitleIndex', {
+    hashKey: 'gameTitle',
+    rangeKey: 'topScore',
+    Projection: { NonKeyAttributes: [ 'wins' ], ProjectionType: 'INCLUDE' } //optional, defaults to ALL
+  });
+});
+
+//Account.createTable(function (err, result) {
+  //if(err) {
+    //console.log('error create table', err);
+  //} else  {
+
+    //console.log(err, result);
+
+    //Account.describeTable(function (err, result) {
+      //console.log('table info', result);
+    //});
+  //}
+//});
+
+vogels.createTables({
+  'Account'   : {readCapacity: 1, writeCapacity: 1},
+  'GameScore' : {readCapacity: 1, writeCapacity: 1}
+}, function (err) {
   if(err) {
-    console.log('error create table', err);
-  } else  {
-
-    console.log(err, result);
-
-    Account.describeTable(function (err, result) {
-      console.log('table info', result);
-    });
+    console.log('Error creating tables', err);
+  } else {
+    console.log('table are now created and active');
   }
 });
