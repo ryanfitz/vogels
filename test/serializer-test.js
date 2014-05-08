48c22d530f2b305cd4809a794ee24aa94020e0ce
Chad Kouse <chad.kouse@gmail.com>
2014-05-08 16:23:28 -0400

    option to ease transition for some users My data has top level arrays stored as strings (JSON encoded).  This option allows the user to tell vogels to convert these strings to sets on the fly.

--- test/serializer-test.js
+++ test/serializer-test.js
@@ -233,7 +233,7 @@ describe('Serializer', function () {
       serializer.serializeItem(schema, {mapArray: mapArray})
         .should.eql({mapArray: {SS: expected}});
     });
-      
+    
     it('should return the original object if it cannot serialize invalid JSON', function() {
       schema.Object('map');
       var circularObj = {};
@@ -451,6 +451,31 @@ describe('Serializer', function () {
       item.mapArray[1].baz[0].should.eql(mapArray[1].baz[0]);
 
     });
+    
+    it('should parse string and convert to ObjectSet attribute', function() {
+      schema.ObjectSet('mapArray', {convertStringToSet:true});
+
+      var mapArray = [
+        {
+          foo: "Bar",
+          baz: ["one", "two", "three"]
+        },
+        {
+          foo: "BarTwo",
+          baz: ["four", "five", "six"]
+        },
+      ];
+      var expected = JSON.stringify(mapArray);
+
+      var item = serializer.deserializeItem(schema, {mapArray: {S: expected}});
+      item.mapArray[0].foo.should.eql(mapArray[0].foo);
+      item.mapArray[0].baz.length.should.eql(mapArray[0].baz.length);
+      item.mapArray[0].baz[0].should.eql(mapArray[0].baz[0]);
+      item.mapArray[1].foo.should.eql(mapArray[1].foo);
+      item.mapArray[1].baz.length.should.eql(mapArray[1].baz.length);
+      item.mapArray[1].baz[0].should.eql(mapArray[1].baz[0]);
+
+    });
       
     it('should parse Object attribute', function() {
         schema.Object('map');
@@ -496,11 +521,33 @@ describe('Serializer', function () {
 
       item.names.should.eql(['Bob', 'Joe', 'Tim']);
     });
+    
+    it('should parse string set attribute', function () {
+      schema.StringSet('names', {convertStringToSet:true});
+
+      var j = JSON.stringify(['Bob', 'Joe', 'Tim']);
+      var itemResp = {names : {S: j} };
+
+      var item = serializer.deserializeItem(schema, itemResp);
+
+      item.names.should.eql(['Bob', 'Joe', 'Tim']);
+    });
 
     it('should parse number set attribute', function () {
       schema.NumberSet('nums');
 
       var itemResp = {nums : {NS: ['18', '22', '23']} };
+
+      var item = serializer.deserializeItem(schema, itemResp);
+
+      item.nums.should.eql([18, 22, 23]);
+    });
+    
+    it('should convert string and parse number set attribute', function () {
+      schema.NumberSet('nums', {convertStringToSet:true});
+
+      var j = JSON.stringify([18, 22, 23]);
+      var itemResp = {nums : {S: j} };
 
       var item = serializer.deserializeItem(schema, itemResp);
 
